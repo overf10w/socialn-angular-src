3e9d40a58f12b17f42624916c488f87f4abb827c
pwnzr <pvvner@yandex.ru>
2017-05-13 18:12:30 +0300

    Login & logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -15,4 +15,25 @@ export class AuthService {
     return this.http.post('http://localhost:3000/user/register', user, {headers: headers})
       .map(res => res.json());
   }
+
+  authenticateUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    // if success, return user info & token
+    return this.http.post('http://localhost:3000/user/authenticate', user, {headers: headers})
+      .map(res => res.json());    
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', user);
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
--- src/app/services/validate.service.ts
+++ src/app/services/validate.service.ts
@@ -7,16 +7,16 @@ export class ValidateService {
 
   validateRegister(user) {
     if (user.name == undefined ||
-        user.email == undefined ||
-        user.username == undefined ||
-        user.password == undefined) {
+      user.email == undefined ||
+      user.username == undefined ||
+      user.password == undefined) {
       return false;
     } else {
-        return true;
-      }
+      return true;
+    }
   }
 
-  validateEmail(email) { 
+  validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
